151ab41a9b4c193a5e32baa63585707bf9fe838c
Horasan <zekiburakhorasan@gmail.com>
2018-03-13 00:22:51 +0300

    V.0.4

---
+++ Assets/Scripts/RayCastTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCastTest : MonoBehaviour
+{
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        Ray ray = new Ray(transform.position, -transform.up);
+        RaycastHit hitInfo;
+		if(Physics.Raycast(ray, out hitInfo, 5)) //boolean döner
+        {
+            Debug.Log(hitInfo.transform.name);
+        }
+        //Debug.DrawRay(transform.position, -transform.up * 10, Color.yellow);
+
+	}
+}
