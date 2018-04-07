f811e5c8d134550923736fbbc38c64c5cee697e3
Horasan <zekiburakhorasan@gmail.com>
2018-04-07 09:54:01 +0300

    V.0.6

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public bool isGameStarted;
+
+	void Start ()
+    {
+        isGameStarted = false;
+    }
+	
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.Return))
+        {
+            StartGame();
+        }
+	}
+
+    private void StartGame()
+    {
+        isGameStarted = true;
+    }
+
+    internal void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,62 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rigitBody;
+    bool lookingRight;
+    GameManager gameManager;
+    Animator animController;
+    public Transform rayOrijin;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+	void Start ()
+    {
+        lookingRight = true;
+        gameManager = FindObjectOfType<GameManager>();
+        animController = GetComponent<Animator>();
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            ChangeDirection();
+        }
+
+        if(transform.position.y < -2)
+        {
+            gameManager.RestartGame();
+        }
 	}
+
+    private void ChangeDirection()
+    {
+        lookingRight = !lookingRight;
+        if(lookingRight)
+        {
+            transform.rotation = Quaternion.Euler(0, 45, 0);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, -45, 0);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(!gameManager.isGameStarted) return;
+
+        else
+        {
+            animController.SetTrigger("gameStarted");
+            rigitBody.position += transform.forward * Time.deltaTime;
+
+            if(!Physics.Raycast(rayOrijin.position, Vector3.down))
+            {
+                animController.SetTrigger("falling");
+            }
+        }
+    }
 }
